8ac9e940d331f41c4bda630835dc4b448e89cbe0
Ivan Leo <ivanleomk@gmail.com>
2024-11-07 18:06:54 +0800

    feat: added tests + new metric/bootstrap func

--- indomee/__init__.py
+++ indomee/__init__.py
@@ -1,4 +1,9 @@
-from rag_metrics.metrics import calculate_mrr, calculate_recall
+from indomee.metrics import calculate_mrr, calculate_recall, calculate_metrics_at_k
+from indomee.bootstrap import bootstrap_sample, bootstrap
 
-
-__all__ = ["calculate_mrr", "calculate_recall"]
+__all__ = [
+    "calculate_mrr",
+    "calculate_recall",
+    "calculate_metrics_at_k",
+    "bootstrap_sample",
+]
